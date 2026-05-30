af8b3f6cceb7ab7ad2546cacaf93574ac448a24b
packetFury <packetfury@pm.me>
2026-05-30 05:16:41 -0500

    Fix null reference in modules/storage/outputs.tf when create_storage is false

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,8 +1,8 @@
 output "storage_account_name" {
-    value = azurerm_storage_account.storage.name
+    value = try(azurerm_storage_account.storage[*].name[0], null)
 }
 
 output "primary_connection_string" {
-    value = azurerm_storage_account.storage.primary_connection_string
+    value = try(azurerm_storage_account.storage[*].primary_connection_string[0], null)
     sensitive = true
 }
