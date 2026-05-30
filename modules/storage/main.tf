174977612cd3bb693ac98bc5a5fa19486033f263
packetFury <packetfury@pm.me>
2026-05-30 04:28:05 -0500

    Organized repo, created Storage Account configuration

---
+++ modules/storage/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "rg" {
+    name = "rg-web-portfolio"
+    location = "East US"
+}
+
+resource "random_string" "unique_id" {
+    length = 6
+    special = false
+    upper = false
+}
+
+resource "azurerm_storage_account" "storage" {
+    name                        = "stor${random_string.unique_id.result}"
+    resource_group_name         = azurerm_resource_group.rg.name
+    location                    = azurerm_resource_group.rg.location
+    account_kind                = "StorageV2"
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "dev"
+    }
+}
---
+++ modules/storage/outputs.tf
@@ -0,0 +1,8 @@
+output "storage_account_name" {
+    value = azurerm_storage_account.storage.name
+}
+
+output "primary_connection_string" {
+    value = azurerm_storage_account.storage.primary_connection_string
+    sensitive = true
+}
